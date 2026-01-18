93a2e670dde7bcbb256abe2cc131b969e1d9c495
Tomer Antebi <121935285+TomerAntebi@users.noreply.github.com>
2026-01-18 11:49:18 +0000

    test

--- src/user_service/service.js
+++ src/user_service/service.js
@@ -11,7 +11,7 @@ app.use(express.json());
 connectToDB(process.env.MONGO_URI);
 
 // 2. Mount the router
-app.use("/users", userController);
+app.use("/", userController);
 
 app.get("/", (req, res) => {
   res.send("<p>ello CodeSandbox!<p>");
